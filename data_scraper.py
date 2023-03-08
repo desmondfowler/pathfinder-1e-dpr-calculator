9479f44b2d545dd2d94b38663a9b9168259eafe9
Desmond <desmondfowler14@gmail.com>
2023-03-08 16:59:10 -0500

    Realized I can do it in one file

--- data_scraper.py
+++ data_scraper.py
@@ -1,8 +1,14 @@
+import json
 import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, quote
+import time
+from tqdm import tqdm
+import re
 
+# Limit requests to AoNPRD so we don't blow them up
+MAX_REQUESTS = 10
 # Change the below flag to True if you want to consider mythic monsters.
 # By default, it is False, since mythic monsters 
 USE_MYTHIC_MONSTERS = True
@@ -27,8 +33,12 @@ except OSError:
     print("Error: Failed to create directory 'enemies'")
     exit(1)
 
+if not os.path.isdir("enemies"):
+    print("Error: 'enemies' is not a directory")
+    exit(1)
+
 # Enemies list
-allEnemies = []
+enemyURLs = []
 
 # Download each page's HTML
 for url in prdUrls:
@@ -37,28 +47,87 @@ for url in prdUrls:
         soup = BeautifulSoup(content, 'html.parser')
         tableElements = soup.select("#main table tr td:first-child a")
         
-        for element in tableElements:
-            
+        for i, element in enumerate(tableElements):
             href = element.get("href")
             encoded = quote(href, safe='=?/()')
 
             enemy = urljoin("https://aonprd.com/", encoded)
             
-            allEnemies.append(enemy)
+            enemyURLs.append(enemy)
 
             
     except requests.exceptions.RequestException as e:
         print(url)
         print(type(e).__name__ + ": " + str(e))
-
-# Sort and remove duplicates
-allEnemies = list(set(allEnemies))
-allEnemies = sorted(allEnemies)
-
+        
 # Write to output file
 try:
-    with open("enemies/enemy_pages.txt", "w") as f:
-        f.write("\n".join(allEnemies))
+    with open("enemies/enemy_pages.json", "w") as f:
+        json.dump(enemyURLs, f)
 except OSError:
-    print("Error: Failed to write to file 'enemies/enemy_pages.txt'")
+    print("Error: Failed to write to file 'enemies/enemy_pages.json'")
     exit(1)
+
+# Start of scraping individual monster html files
+enemiesByCR = {}
+#testing number:
+LOOPS = 500
+
+for i, url in enumerate(tqdm(enemyURLs)):
+    t = time.time()
+    try:
+        html = requests.get(url).text
+    except requests.exceptions.RequestException as e:
+        print(url)
+        print(type(e).__name__ + ": " + str(e))
+    tSpent = time.time() - t
+    if tSpent < 1 / MAX_REQUESTS:
+        time.sleep(1 / MAX_REQUESTS - tSpent)
+    
+    # Clean up HTML
+    regex = r'(?:\r\n|\r|\xad|' + chr(10) + ')+'
+    html = re.sub(r'(\s+)' + regex + r'|' + regex + r'(?=\s+)', r'\1', html) 
+    html = re.sub(regex, r' ', html) 
+    html = re.sub(r'(<br/?)\s*/?>', r'\1>', html) # Fix broken <br> tags
+    html = re.sub(r'[−—–‐‑‒―]|&ndash;|&mdash;', "-", html) 
+
+    #Parse the html using soup
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    #Find the monster's title tag which houses the name and CR value
+    monsterTitleTag = soup.find('h2', {'class': 'title'})
+    
+    # Extract the text
+    monsterTitle = monsterTitleTag.text
+    
+    # Extract the CR value from the text
+    crMatch = re.search(r'CR\s+(\d+(/(\d+))?|\d+)', monsterTitle)
+    if crMatch:
+        cr = crMatch.group(0)
+    else:
+        cr = None
+    
+    # Extract additional monster attributes
+    monsterAttrs = {}
+    
+    
+    # Add the monster to the dictionary for its CR
+    if cr is not None:
+        if cr not in enemiesByCR:
+            enemiesByCR[cr] = []
+        enemiesByCR[cr].append(monsterTitle)
+        
+
+
+    # Write to output json file
+    try:
+        with open("enemies/enemy_info.json", "w") as f:
+            json.dump(enemiesByCR, f)
+    except OSError:
+        print("Error: Failed to write to file 'enemies/enemy_info.json'")
+        exit(1)
+    
+    # For testing, amount of loops:
+    # if i >= LOOPS:
+    #     print("Hit " + str(LOOPS) + " loops, exiting")
+    #     exit()
